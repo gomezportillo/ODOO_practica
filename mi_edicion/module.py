a4075072d3b3008836dea539ab8fa1fcc74d3a31
gomezportillo <pedroma.almagro@gmail.com>
2016-05-22 19:41:34 +0200

    :fire: misc changes

--- mi_edicion/module.py
+++ mi_edicion/module.py
@@ -8,11 +8,10 @@ class edicion(orm.Model):
 	_description = "Ediciones de cursos"
 	_columns = {
 		'is_course':fields.boolean("¿Es una edicion de curso?"),
-		#'curso' : fields.one2one('res.curso', 'Curso')
-		'profesores' : fields.many2one('hr.employee', "Profesor"),
-		'fecha' : fields.date('Fecha')
+		#'curso' : fields.many2one('res.curso', 'Curso') #para saber de qué curso es, no?
+		'profesor' : fields.many2one('hr.employee', "Profesor"),
+		#'fecha_inicio' : fields.date('Fecha inicio'), #cuando vaya el resto probamos esto
+		#'fecha_fin' : fields.date('Fecha fin')
 	}
 
 edicion()
-
-#'edicion'  : fields.many2one('product.product', "Edicion", domain="[{'sale_ok','=','1'}]"),
