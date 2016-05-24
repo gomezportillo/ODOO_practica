5e05728fc922b8b5a516f416b500669f0417ca01
gomezportillo <pedroma.almagro@gmail.com>
2016-05-24 20:11:02 +0200

    :+1: module "edicion" (virtually) working

--- mi_edicion/__openerp__.py
+++ mi_edicion/__openerp__.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
-
 {
     "name" : "Gestión de ediciones",
     "version" : "2.2",
     "author" : "Pedro Manuel Gómez-Portillo & José Antonio Martínez",
     "category" : "Generic Modules/Others",
-    "website" : "https://github.com/gomezportillo/odoo_practica",
+    "website" : "http://www.github.com/gomezportillo",
     "description": "Módulo para la gestión de ediciones",
-    "depends" : ["base"],
+    "depends" : ["product, hr"],
     "update_xml" : ["edicion_view.xml"],
     "active": True,
     "installable": True
--- mi_edicion/module.py
+++ mi_edicion/module.py
@@ -3,15 +3,15 @@ from openerp.osv import fields, orm
 
 class edicion(orm.Model):
 
-	#	_name = "res.edicion"
-	_inherit = "product.template"
-	_description = "Ediciones de cursos"
-	_columns = {
-		'is_course':fields.boolean("¿Es una edicion de curso?"),
-		#'curso' : fields.many2one('res.curso', 'Curso') #para saber de qué curso es, no?
-		'profesor' : fields.many2one('hr.employee', "Profesor"),
-		#'fecha_inicio' : fields.date('Fecha inicio'), #cuando vaya el resto probamos esto
-		#'fecha_fin' : fields.date('Fecha fin')
-	}
+  #  _name = "res.edicion"
+  _inherit = "product.template"
+  _description = "Ediciones de cursos"
+  _columns = {
+    'is_course': fields.boolean("Es edicion"),
+	'profesor' : fields.many2one('hr.employee', "Profesor")
+    'fecha_in' : fields.date('Fecha inicio'),
+	'fecha_fin' : fields.date('Fecha fin')
+    #'curso' : fields.many2one('res.curso', 'Curso') #para saber de qué curso es, no?
+  }
 
 edicion()
